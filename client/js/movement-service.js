83779897be5019f320392223dc6a7ae961658d66
Macarena Rodriguez <maaaki.rodriguez@gmail.com>
2021-06-16 21:10:46 -0300

    Creo vista de egresos

--- client/js/movement-service.js
+++ client/js/movement-service.js
@@ -12,6 +12,12 @@ async function getIncomes() {
     return movements;
 }
 
+async function getExpense() {
+    const resp = await fetch(`${BASE_URL}/movements?type=expense`);
+    const { movements } = await resp.json();
+    return movements;
+}
+
 async function update(movement) {
     const resp = await fetch(`${BASE_URL}/movements/${movement.id}`, {
         method: 'PUT',
@@ -63,4 +69,5 @@ export default {
     remove,
     getLast,
     getIncomes,
+    getExpense,
 };
